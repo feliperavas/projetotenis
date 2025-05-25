b579d8691eb14a1ea25bda59e799d960594914fd
felipe teodoro ravagnani <felipeteodororavagnani@gmail.com>
2025-05-25 15:55:06 -0300

    update reponsividade

--- script/script.js
+++ script/script.js
@@ -9,8 +9,5 @@ video.addEventListener("ended", () => {
 });
 
 
-function toggleMenu() {
-    const menu = document.getElementById("mobileMenu");
-    menu.style.display = (menu.style.display === "block") ? "none" : "block";
-}
+
 
