a32dd579f4ae38c5fdee89f5574ffa7820ec8a97
felipe teodoro ravagnani <felipeteodororavagnani@gmail.com>
2025-05-28 13:10:23 -0300

    tentativa 1 reponsivo menu

--- assets/javascript/index/script.js
+++ assets/javascript/index/script.js
@@ -8,6 +8,15 @@ video.addEventListener("ended", () => {
     }, 30000); // 30 segundos em milissegundos
 });
 
+//Reponsivo menu
+
+  const toggle = document.getElementById('menu-toggle');
+  const menu = document.getElementById('menu');
+
+  toggle.addEventListener('click', () => {
+    menu.classList.toggle('active');
+  });
+
 
 
 
